70c4595881bb143ed7496e62a355f757fc87dee1
kuifje0257 <kuifje0257@hotmail.be>
2021-11-30 09:42:44 +0100

    all folders and basic files

---
+++ build/CMakeFiles/Opdracht4_cl.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Opdracht4_cl.dir/echo_Client.c.o"
+  "Opdracht4_cl"
+  "Opdracht4_cl.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Opdracht4_cl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/Opdracht4_sv.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Opdracht4_sv.dir/echo_Server.c.o"
+  "Opdracht4_sv"
+  "Opdracht4_sv.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Opdracht4_sv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
